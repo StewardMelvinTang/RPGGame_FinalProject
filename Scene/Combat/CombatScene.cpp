49c3bb9f0e89a9112f2340053ff3502b4153b580
Steward Melvin Tang <melvintanggames@gmail.com>
2024-06-23 02:03:24 +0800

    added combat avatar for combat arena scene
    1. added combat PSD in _Resource/UIDesign

--- Scene/Combat/CombatScene.cpp
+++ Scene/Combat/CombatScene.cpp
@@ -40,22 +40,22 @@ void CombatScene::Initialize() {
     //Button Size
     int Btn_w = 230;
     int Btn_h = 65;
-	// AddNewObject(new Engine::Image("bg/Combat_bg.png", 0 ,0 , 1600, 832, 0.0, 0.0));
+	AddNewObject(new Engine::Image("bg/battlearena_bg.png", 0 ,0 , 1600, 832, 0.0, 0.0));
     
 	// AddNewObject(new Engine::Label("Combat Scene!", "pixel-font.ttf", 150, halfW, halfH - 175, 255, 255, 255, 255, 0.5, 0.5)); // 0.5 is the anchor for both x and y
     // AddNewObject(new Engine::Image("btn/buttonSquare_beige_pressed.png", halfW/4 , halfH*2, 600, 200, 0.5, 0.5));
     Engine::ImageButton* attack;
-    attack = new Engine::ImageButton("btn/btn_attack.png", "btn/btn_attack_hover.png", (halfW - (Btn_w*0.96)*3.07), halfH - (Btn_h*0.96) + 350, Btn_w*0.96, Btn_h*0.96);
+    attack = new Engine::ImageButton("btn/btn_attack.png", "btn/btn_attack_hover.png", 865, 593, 270, 64);
     attack->SetOnClickCallback(bind(&CombatScene::AttackOnClick, this)); 
     AddNewControlObject(attack);
 
     Engine::ImageButton* items;
-    items = new Engine::ImageButton("btn/btn_items.png", "btn/btn_items_hover.png", (halfW - (Btn_w*0.96)*3.07) + Btn_w*0.96*1.05, halfH - (Btn_h*0.96) + 350, Btn_w*0.96, Btn_h*0.96);
+    items = new Engine::ImageButton("btn/btn_items.png", "btn/btn_items_hover.png", 865, 664, 274, 64);
     items->SetOnClickCallback(bind(&CombatScene::ItemsOnClick, this)); 
     AddNewControlObject(items);
 
     Engine::ImageButton* escape;
-    escape = new Engine::ImageButton("btn/btn_escape.png", "btn/btn_escape_hover.png", (halfW - (Btn_w*0.96)*3.07) + (Btn_w*0.96)*2.10, halfH - (Btn_h*0.96) + 350, Btn_w*0.96, Btn_h*0.96);
+    escape = new Engine::ImageButton("btn/btn_escape.png", "btn/btn_escape_hover.png", 865, 733, 274, 64);
     escape->SetOnClickCallback(bind(&CombatScene::EscapeOnClick, this)); 
     AddNewControlObject(escape);
 
@@ -66,39 +66,39 @@ void CombatScene::Initialize() {
     std::cout<<"Max_HP: " << maxHP << endl;
 
     //Player Health Values:
-    int x = (halfW - (Btn_w*0.96)*3.07);
+        int x = (halfW - (Btn_w*0.96)*3.07);
     int y = halfH - (Btn_h*0.96) + 275;
-    HP_BarBG = new Engine::Image("bg/progressbar_bg.png", x, y, 282, 40 );
-    HP_BarFILL = new Engine::Image("bg/progressbar_fill.png", x, y, 282, 40);
+    HP_BarBG = new Engine::Image("bg/progressbar_bg.png", 20, 305, 282, 40);
+    HP_BarFILL = new Engine::Image("bg/progressbar_fill.png", 20, 305, 282, 40);
     HP_BarFILL->Size.x = 282 * (currentHP / maxHP);
-    TXT_HPVal = new Engine::Label("HP: " + to_string(static_cast<int>(round(currentHP))), "pixel-font.ttf", 30, x + 8, y + 22, 255, 255, 255, 255, 0.0, 0.5);
+    TXT_HPVal = new Engine::Label("HP: " + to_string(static_cast<int>(round(currentHP))), "pixel-font.ttf", 30, 28, 327, 255, 255, 255, 255, 0.0, 0.5);
     AddNewObject(HP_BarBG);
     AddNewObject(HP_BarFILL);
     AddNewObject(TXT_HPVal);
 
     //Player Attack Values:
 
-    ATK_BarBG = new Engine::Image("bg/progressbar_bg.png", x + 325, y, 282, 40 );
-    ATK_BarFILL = new Engine::Image("bg/atkbar_fill.png", x + 325, y, ATK_FILL, 40);
-    TXT_ATKVal = new Engine::Label("ATK: " + to_string(static_cast<int>(round(playerATK))), "pixel-font.ttf", 30, x + 333, y + 22, 255, 255, 255, 255, 0.0, 0.5);
+    ATK_BarBG = new Engine::Image("bg/progressbar_bg.png", 20, 353, 282, 40 );
+    ATK_BarFILL = new Engine::Image("bg/atkbar_fill.png", 20, 353, ATK_FILL, 40);
+    TXT_ATKVal = new Engine::Label("ATK: " + to_string(static_cast<int>(round(playerATK))), "pixel-font.ttf", 30, 23 , 375, 255, 255, 255, 255, 0.0, 0.5);
     AddNewObject(ATK_BarBG);
     AddNewObject(ATK_BarFILL);
     AddNewObject(TXT_ATKVal);
 
     //<-----ENEMY VALUES:----->
     //Enemy Health Values:
-    Enemy_HP_BarBG = new Engine::Image("bg/progressbar_bg.png", x, y-550, 282, 40);
-    Enemy_HP_BarFILL = new Engine::Image("bg/progressbar_fill.png", x, y-550, 282, 40);
-    Enemy_TXT_HPVal = new Engine::Label("HP: " + to_string(static_cast<int>(round(Enemy_currentHP))), "pixel-font.ttf", 30, x + 8, y - 528, 255, 255, 255, 255, 0.0, 0.5);
+    Enemy_HP_BarBG = new Engine::Image("bg/progressbar_bg.png", 1303, 34, 282, 40);
+    Enemy_HP_BarFILL = new Engine::Image("bg/progressbar_fill.png", 1303, 34, 282, 40);
+    Enemy_TXT_HPVal = new Engine::Label("HP: " + to_string(static_cast<int>(round(Enemy_currentHP))), "pixel-font.ttf", 30, 1310, 37, 255, 255, 255, 255, 0.0, 0.5);
     AddNewObject(Enemy_HP_BarBG);
     AddNewObject(Enemy_HP_BarFILL);
     AddNewObject(Enemy_TXT_HPVal);
 
     //Enemy Attack Values:
 
-    Enemy_ATK_BarBG = new Engine::Image("bg/progressbar_bg.png", x + 325, y-550, 282, 40 );
-    Enemy_ATK_BarFILL = new Engine::Image("bg/atkbar_fill.png", x + 325, y-550, ATK_FILL, 40);
-    Enemy_TXT_ATKVal = new Engine::Label("ATK: " + to_string(static_cast<int>(round(Enemy_ATK))), "pixel-font.ttf", 30, x + 333, y - 528, 255, 255, 255, 255, 0.0, 0.5);
+    Enemy_ATK_BarBG = new Engine::Image("bg/progressbar_bg.png", 1303, 82, 282, 40 );
+    Enemy_ATK_BarFILL = new Engine::Image("bg/atkbar_fill.png", 1303, 82, ATK_FILL, 40);
+    Enemy_TXT_ATKVal = new Engine::Label("ATK: " + to_string(static_cast<int>(round(Enemy_ATK))), "pixel-font.ttf", 30, 1310, 85, 255, 255, 255, 255, 0.0, 0.5);
     AddNewObject(Enemy_ATK_BarBG);
     AddNewObject(Enemy_ATK_BarFILL);
     AddNewObject(Enemy_TXT_ATKVal);
@@ -106,9 +106,11 @@ void CombatScene::Initialize() {
 
 
     //Sprite and Visuals
-    Platform = new Engine::Image("bg/buttonSquare_blue.png", x + 1150, items->Position.y, 282, 160, 0.5, 0.5);
-    AddNewObject(Platform);
-    PlayerSprite = new Engine::Image("char/char_idle_up.png", x + 1150, items->Position.y, 282, 160, 0.5, 0.5);
+    // Platform = new Engine::Image("bg/buttonSquare_blue.png", x + 1150, items->Position.y, 282, 160, 0.5, 0.5);
+    // AddNewObject(Platform);
+
+    std::string avatarFilePath = "avatar/avatar_" + to_string(Engine::GameEngine::GetInstance().GetCurrentActivePlayer().avatarID + 1) + "_combat.png";
+    PlayerSprite = new Engine::Image(avatarFilePath, 104, 440, 413, 392, 0, 0);
     AddNewObject(PlayerSprite);
 
     EnemySprite = new Engine::Image("enemy/Enemy1Sprite.png", x + 1150, Enemy_ATK_BarBG->Position.y, 282*4, 160*4, 0.5, 0.5);
